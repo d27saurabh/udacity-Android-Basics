8cf6ac329072374a9085d370a0a64bfeb513a814
pipboy <farciebe@gmail.com>
2017-12-11 18:15:53 +0200

    pet URIs in contract class

--- Pets/app/src/main/java/com/example/android/pets/data/PetContract.java
+++ Pets/app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -1,5 +1,6 @@
 package com.example.android.pets.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 public final class PetContract {
@@ -7,11 +8,16 @@ public final class PetContract {
     private PetContract() {
     }
 
+    public static final String CONTENT_AUTHORITY = "com.example.android.pets";
+    public static final String PATH_PETS = "pets";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
     public static final class PetEntry implements BaseColumns {
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PETS);
 
         public static final String TABLE_NAME = "pets";
-
         public static final String _ID = BaseColumns._ID;
+
         public static final String COLUMN_PET_NAME = "name";
         public static final String COLUMN_PET_BREED = "breed";
         public static final String COLUMN_PET_GENDER = "gender";
@@ -20,5 +26,6 @@ public final class PetContract {
         public static final int GENDER_UNKNOWN = 0;
         public static final int GENDER_MALE = 1;
         public static final int GENDER_FEMALE = 2;
+
     }
 }
