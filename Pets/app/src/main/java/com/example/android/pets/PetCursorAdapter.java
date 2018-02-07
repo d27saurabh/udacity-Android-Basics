55b60703b00be423799794f49496da1a838eb1a3
pipboy <farciebe@gmail.com>
2018-02-07 15:01:41 +0200

    if a pet has unknown breed it shows it in UI -- app finished()

--- Pets/app/src/main/java/com/example/android/pets/PetCursorAdapter.java
+++ Pets/app/src/main/java/com/example/android/pets/PetCursorAdapter.java
@@ -2,6 +2,7 @@ package com.example.android.pets;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -58,6 +59,10 @@ public class PetCursorAdapter extends CursorAdapter {
 
         String cName = cursor.getString(cursor.getColumnIndex(PetEntry.COLUMN_PET_NAME));
         String cSummary = cursor.getString(cursor.getColumnIndex(PetEntry.COLUMN_PET_BREED));
+
+        if (TextUtils.isEmpty(cSummary)){
+            cSummary = context.getString(R.string.unknown_breed);
+        }
 
         name.setText(cName);
         summary.setText(cSummary);
